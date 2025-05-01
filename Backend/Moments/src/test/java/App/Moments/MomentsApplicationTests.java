349404a69f04676ae3e7ec6d2b2a2f9c27543433
Pedro-Cardoso7 <105787134+Pedro-Cardoso7@users.noreply.github.com>
2025-05-01 16:01:35 +0100

    Init

---
+++ Backend/Moments/src/test/java/App/Moments/MomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package App.Moments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
