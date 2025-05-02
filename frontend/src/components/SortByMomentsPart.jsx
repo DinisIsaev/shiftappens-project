a9ce48c51c0bb6342d6497cf8ea899919ed54c32
Dinis Isaev <dinisisaev@gmail.com>
2025-05-02 23:23:27 +0100

    stuff

---
+++ frontend/src/components/SortByMomentsPart.jsx
@@ -0,0 +1,12 @@
+function SortByMomentsPart(){
+    return (
+        <div className="sort-by-moments-card">
+            <p className="year-identifier">Year</p>
+            <div className="moments-year-part">
+                {/*MAPPING TO GET ALL MOMENTS OF CERTAIN YEAR. USE ALBUMCARD*/}
+            </div>
+        </div>
+    );
+}
+
+export default SortByAlbumsPart;
